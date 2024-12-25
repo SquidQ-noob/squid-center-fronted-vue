ff0d6cb6fb3726bde9a6d572b2a992daf4627a6d
Q. <Squid_2021@163.com>
2024-12-25 14:07:11 +0800

    first-commit

---
+++ user-center-frontend-vue/src/pages/admin/UserManagePage.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="userManagePage">
+    <a-input-search
+      style="max-width: 320px; margin-bottom: 20px"
+      v-model:value="searchValue"
+      placeholder="输入用户名搜索"
+      enter-button="搜索"
+      size="large"
+      @search="onSearch"
+    />
+    <a-table :columns="columns" :data-source="data">
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'avatarUrl'">
+          <a-image :src="record.avatarUrl" :width="120" />
+        </template>
+        <template v-else-if="column.dataIndex === 'userRole'">
+          <div v-if="record.userRole === 1">
+            <a-tag color="green">管理员</a-tag>
+          </div>
+          <div v-else>
+            <a-tag color="blue">普通用户</a-tag>
+          </div>
+        </template>
+        <template v-else-if="column.dataIndex === 'createTime'">
+          {{ dayjs(record.createTime).format("YYYY-MM-DD HH:mm:ss") }}
+        </template>
+        <template v-else-if="column.key === 'action'">
+          <a-button danger @click="doDelete(record.id)">删除</a-button>
+        </template>
+      </template>
+    </a-table>
+  </div>
+</template>
+<script lang="ts" setup>
+import { deleteUser, searchUsers } from "@/api/user";
+import { ref } from "vue";
+import { message } from "ant-design-vue";
+import dayjs from "dayjs";
+
+const searchValue = ref("");
+// 获取数据
+const onSearch = () => {
+  fetchData(searchValue.value);
+};
+
+// 删除数据
+const doDelete = async (id: string) => {
+  if (!id) {
+    return;
+  }
+  const res = await deleteUser(id);
+  if (res.data.code === 0) {
+    message.success("删除成功");
+  } else {
+    message.error("删除失败");
+  }
+};
+
+const columns = [
+  {
+    title: "id",
+    dataIndex: "id",
+  },
+  {
+    title: "用户名",
+    dataIndex: "username",
+  },
+  {
+    title: "账号",
+    dataIndex: "userAccount",
+  },
+  {
+    title: "头像",
+    dataIndex: "avatarUrl",
+  },
+  {
+    title: "性别",
+    dataIndex: "gender",
+  },
+  {
+    title: "创建时间",
+    dataIndex: "createTime",
+  },
+  {
+    title: "用户角色",
+    dataIndex: "userRole",
+  },
+  {
+    title: "操作",
+    key: "action",
+  },
+];
+
+// 数据
+// 当你在ref魔法盒子中放入一个值时，Vue.js会创建一个带有.value属性的对象，并通过Proxy代理这个对象。
+// 当你访问或修改.value时，Vue.js能够检测到变化，并触发界面更新
+// 你将一个对象放入reactive魔法盒子时，Vue.js会通过Proxy代理这个对象的所有属性。
+// 当你访问或修改这些属性时，Vue.js能够检测到变化，并触发界面更新
+const data = ref([]); // 响应式变量
+
+// 获取数据
+const fetchData = async (username = "") => {
+  const res = await searchUsers(username);
+  if (res.data.data) {
+    data.value = res.data.data;
+  } else {
+    message.error("获取数据失败");
+  }
+};
+
+fetchData();
+</script>
